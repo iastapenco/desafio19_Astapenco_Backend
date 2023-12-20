5a3b62b27b3a46d4eeff38c9b05157ffc03d691b
Iván Astapenco <iastapenco@gmail.com>
2023-12-20 16:10:10 -0300

    se crea funcionalidad de recuperación de contraseña

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,56 @@
 import { Router } from "express";
+import crypto from "crypto";
+import { sendRecoveryMail } from "../config/nodemailer.js";
 import UserManager from "../dao/managers_mongo/userManagerMongo.js";
 
 const userRouter = Router();
+const recoveryLinks = {};
 const userManager = new UserManager();
+
+userRouter.post("/password-recovery", (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const token = crypto.randomBytes(20).toString("hex");
+
+    recoveryLinks[token] = {
+      email: email,
+      timestamp: Date.now(),
+    };
+
+    const recoveryLink = `http://localhost:8080/api/users/reset-password/${token}`;
+
+    sendRecoveryMail(email, recoveryLink);
+
+    res.status(200).send("Correo de recuperación enviado");
+  } catch (error) {
+    res.status(500).send(`Error al enviar el mail ${error}`);
+  }
+});
+
+userRouter.post("/reset-password/:token", (req, res) => {
+  const { token } = req.params;
+  const { newPassword, newPassword2 } = req.body;
+
+  try {
+    const linkData = recoveryLinks[token];
+    if (linkData && Date.now() - linkData.timestamp <= 3600000) {
+      const { email } = linkData;
+
+      if (newPassword == newPassword2) {
+        delete recoveryLinks[token];
+
+        res.status(200).send("Contraseña modificada correctamente");
+      } else {
+        res.status(400).send("Las contraseñas deben ser idénticas");
+      }
+    } else {
+      res.status(400).send("Token inválido o expirado");
+    }
+  } catch (error) {
+    res.status(500).send(`Error al modificar contraseña ${error}`);
+  }
+});
 
 userRouter.get("/", async (req, res) => {
   try {
